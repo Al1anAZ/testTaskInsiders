ffdc30b61a322e177555cc098556602b06f459f0
Artem <artemzagovora@gmail.com>
2024-11-15 13:30:50 +0200

    made def structure, connected dep

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashBoard",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
---
+++ src/app/[tab]/page.tsx
@@ -0,0 +1,6 @@
+import styles from "./style.module.scss";
+
+const Tab = () => {
+  return <main className={styles.tab}></main>;
+};
+export default Tab;
